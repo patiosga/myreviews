591d32a108657a8a0be201d043ba05107a751301
Gergios Dimou <georgdimou@csd.auth.gr>
2022-11-17 15:16:10 +0200

    date class

---
+++ src/api/date.java
@@ -0,0 +1,9 @@
+package api;
+
+import java.text.SimpleDateFormat;
+import java.util.Date ;
+
+public class date {
+    Date thisDate = new Date();
+    SimpleDateFormat dateForm =  new SimpleDateFormat("dd/MMMM/y");
+}
