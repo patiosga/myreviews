4ca20d72d3b254ced3eb4ec675317fe253174bd6
Gergios Dimou <georgdimou@csd.auth.gr>
2022-11-17 15:36:47 +0200

    Location class

---
+++ src/api/Location.java
@@ -0,0 +1,44 @@
+package api;
+
+public class Location {
+    private String address;
+    private String town;
+    private String postCode;
+
+
+    public Location(String address, String town, String postCode) {
+        this.address = address;
+        this.town = town;
+        this.postCode = postCode;
+    }
+
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+
+}
