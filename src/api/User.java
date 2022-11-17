2875139a1e956f78c920ac5995b5baf69496741f
Gergios Dimou <georgdimou@csd.auth.gr>
2022-11-17 16:31:52 +0200

    User class

---
+++ src/api/User.java
@@ -0,0 +1,40 @@
+package api;
+
+public class User {
+
+    protected String userName;
+    protected String password;
+    protected String type;
+
+    public User(String userName, String password, String type) {
+        this.userName = userName;
+        this.password = password;
+        this.type = type;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
