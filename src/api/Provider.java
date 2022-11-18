3b0ac0383665e2a6e7e81141da8c55d0bc0c7116
Gergios Dimou <georgdimou@csd.auth.gr>
2022-11-18 20:21:30 +0200

    Provider class

---
+++ src/api/Provider.java
@@ -0,0 +1,16 @@
+package api;
+
+import java.util.ArrayList;
+
+public class Provider extends User{
+
+    //private ArrayList<RegistAccommod> listAccommod;
+
+    public Provider(String userName, String password, String type) {
+        super(userName, password, type);
+    }
+
+    public void display(){
+        //provolh katalumatwn-dunatothta epejergasias
+    }
+}
