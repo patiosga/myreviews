6862e4f7d2d730ff563f04b3f499e7f91d992c2d
Gergios Dimou <georgdimou@csd.auth.gr>
2022-11-18 20:02:53 +0200

    SimpleUser class

---
+++ src/api/SimpleUser.java
@@ -0,0 +1,20 @@
+package api;
+
+import java.util.ArrayList;
+
+public class SimpleUser extends User{
+
+    //private ArrayList<Evaluation> listEvaluations;
+
+
+    public SimpleUser(String userName, String password, String type) {
+        super(userName, password, type);
+    }
+
+    //public float getAverageOfRatings() {
+    // }!!!!!!!!
+
+    public void display(){
+        //probolh katalimatwn
+    }
+}
