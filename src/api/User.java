cdd0fc580aa54a578f3d0b23385d55173d45767e
Georgios Patios <patiosga@csd.auth.gr>
2022-11-22 18:11:08 +0200

    houston all clear for now

--- src/api/User.java
+++ src/api/User.java
@@ -2,24 +2,23 @@ package api;
 
 public abstract class User { //βγάζει νόημα να είναι abstract γιατί δεν πρόκειται να δημιουργηθούν αντικείμενα User
     // εδώ ίσως το αλλάξουμε ώστε να έχουμε και τα όνομα, επίθετο σε ξεχωριστές μεταβλητές αλλά μπορεί και να μη χρειαστεί αν υπάρχει καλή μέθοδος parsing για string
-    protected String userName;
+    protected final String firstName;
+    protected final String lastName;
+    protected final String userName;
     protected String password;
-    protected String type;
+    protected final String type;
 
-    public User(String userName, String password, String type) {
+    public User(String firstName, String lastName, String userName, String password, String type) {
         this.userName = userName;
         this.password = password;
         this.type = type;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public String getUserName() {
         return userName;
     }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
 
     public String getPassword() {
         return password;
@@ -29,17 +28,18 @@ public abstract class User { //βγάζει νόημα να είναι abstract 
         this.password = password;
     }
 
-
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getFirstName() { //για εύκολη χρήση στην εμφάνιση του μικρού ονόματος του χρήστη στην αξιολόγηση
-        String[] splitNames = userName.split(" ",2);
-        return splitNames[0];
+    public String getLastName() {
+        return lastName;
     }
+
+
+
 }
