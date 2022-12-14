7faa5ddcffd9850c8498b9805e57f5a21ec835f5
Georgios Patios <patiosga@csd.auth.gr>
2022-12-14 18:55:05 +0200

    Hope this works

---
+++ test/api/LocationTest.java
@@ -0,0 +1,37 @@
+package api;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LocationTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getAddress() {
+    }
+
+    @Test
+    public void setAddress() {
+    }
+
+    @Test
+    public void getTown() {
+    }
+
+    @Test
+    public void setTown() {
+    }
+
+    @Test
+    public void getPostCode() {
+    }
+
+    @Test
+    public void setPostCode() {
+    }
+}
