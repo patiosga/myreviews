e8e1e7fea1e3de781f3851633aad07ee31ff3f98
Georgios Dimou <georgdimou@csd.auth.gr>
2022-12-14 18:53:31 +0200

    kati

---
+++ test/api/AccomodationTest.java
@@ -0,0 +1,76 @@
+package api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccomodationTest {
+
+    @Test
+    public void getNumOfRatings() {
+    }
+
+    @Test
+    public void setNumOfRatings() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void setDescription() {
+    }
+
+    @Test
+    public void getStayType() {
+    }
+
+    @Test
+    public void setStayType() {
+    }
+
+    @Test
+    public void getPlace() {
+    }
+
+    @Test
+    public void setPlace() {
+    }
+
+    @Test
+    public void getProvider() {
+    }
+
+    @Test
+    public void getSingularId() {
+    }
+
+    @Test
+    public void getAvgRating() {
+    }
+
+    @Test
+    public void getTotalEvaluations() {
+    }
+
+    @Test
+    public void setTotalEvaluations() {
+    }
+
+    @Test
+    public void updateAvgRatingOfAccomodation() {
+    }
+
+    @Test
+    public void addUtilityType() {
+    }
+}
---
+++ test/api/EvaluationTest.java
@@ -0,0 +1,49 @@
+package api;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EvaluationTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getEvaluationText() {
+    }
+
+    @Test
+    public void setEvaluationText() {
+    }
+
+    @Test
+    public void getGrade() {
+    }
+
+    @Test
+    public void setGrade() {
+    }
+
+    @Test
+    public void getUser() {
+    }
+
+    @Test
+    public void getAccomodation() {
+    }
+
+    @Test
+    public void getCurrentDate() {
+    }
+
+    @Test
+    public void setCurrentDate() {
+    }
+
+    @Test
+    public void display() {
+    }
+}
