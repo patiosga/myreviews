f0de54f4fa7ea7f7808aad474a49c71ffc1696f6
Georgios Patios <giorgospatios@gmail.com>
2022-12-14 18:29:01 +0200

    Fixed lib directory

--- test/api/LocationTest.java
+++ test/api/LocationTest.java
@@ -1,42 +1,37 @@
 package api;
 
-import api.Location;
-import org.junit.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class LocationTest {
 
-
-    Location loc;
-
-    Location instance;
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
     @Before
-    public void setUp() {
-        instance = new Location("Strathgou Dagkli", "Thessaloniki", "12121");
+    public void setUp() throws Exception {
     }
-
-    @After
-    public void tearDown() {
-    }
-
 
     @Test
-    public void getSet() {
-        assertEquals("Strathgou Dagkli", instance.getAddress());
-        assertEquals("Thessaloniki", instance.getTown());
-        assertEquals("12121", instance.getPostCode());
+    public void getAddress() {
     }
 
+    @Test
+    public void setAddress() {
+    }
 
+    @Test
+    public void getTown() {
+    }
+
+    @Test
+    public void setTown() {
+    }
+
+    @Test
+    public void getPostCode() {
+    }
+
+    @Test
+    public void setPostCode() {
+    }
 }
