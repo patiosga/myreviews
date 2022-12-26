a53bf8b56f5658184d0b19a44512c1daa77c1f09
Georgios Dimou <georgdimou@csd.auth.gr>
2022-12-27 01:36:30 +0200

    douleuei?

---
+++ test/api/SimpleUserTest.java
@@ -0,0 +1,18 @@
+package api;
+
+import static org.junit.Assert.*;
+
+public class SimpleUserTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void updateAvgRatingOfUser() {
+    }
+
+    @org.junit.Test
+    public void getAvgRating() {
+    }
+}
