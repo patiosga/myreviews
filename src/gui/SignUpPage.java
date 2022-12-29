8cc48a3446e9c3d63f75b6a616be68cff99d4f5c
Georgios Dimou <georgdimou@csd.auth.gr>
2022-12-30 00:35:12 +0200

    arxh SignUpPage

---
+++ src/gui/SignUpPage.java
@@ -0,0 +1,21 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import api.ManageUsers;
+
+public class SignUpPage extends JFrame implements ActionListener {
+
+    private ManageUsers usersManage;
+
+    private JButton
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+
+}
