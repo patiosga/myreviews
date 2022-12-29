4f844a2afadc150b4dad3f2058308b60101e694f
Georgios Dimou <georgdimou@csd.auth.gr>
2022-12-29 15:34:52 +0200

    LoginPage mexri ena shmeio

---
+++ src/gui/LoginPage.java
@@ -0,0 +1,60 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+
+
+public class LoginPage extends JFrame implements ActionListener {
+
+
+
+
+    private JButton userName,password;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    public void LoginP(){
+        JPanel panel = new JPanel();
+        setSize(350,200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Login");
+        this.add(panel);
+
+        panel.setLayout(null);
+
+        JLabel userLabel = new JLabel("Username");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100,20,165,25 );
+        panel.add(userText);
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10,50,80,25);
+        panel.add(passwordLabel);
+        JPasswordField passwordText = new JPasswordField();
+        passwordText.setBounds(100,50,165,25);
+        panel.add(passwordText);
+
+        JButton button = new JButton("Login");
+        button.setBounds(10,80,80,25);
+        panel.add(button);
+
+
+
+        this.setVisible(true);
+
+    }
+
+
+
+
+
+
+}
